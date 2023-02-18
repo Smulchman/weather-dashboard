a8a4168d4db821befd7c4275ff83f6e70c97ce3f
Sam Mulcahy <mulch@Sams-MacBook-Pro.local>
2023-02-18 11:30:00 -0600

    used search input data to retrieve lattitude and longitude data for the weather data search

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,53 @@
-var cityList = $('#city-list');
-
+var cityList = document.querySelector('#city-list');
 // '+{statecode}+', including this in the URL allows us to specify the state we're searching in.
 
 var cityCords;
 var searchBtn = $('#search-btn');
 var searchInputEl = $('input[id="search-input"]');
 
+function fillForecast(lat, lon){
+    var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&units=imperial&appid=3d51ff48e9ee8482ed210ebd0d533bdc';
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        
+    })
+};
 
 searchBtn.on('click', function(){
     var cityName = searchInputEl.val();
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=3d51ff48e9ee8482ed210ebd0d533bdc';
+    cityName = cityName.replace(/ /g, '');
+    var cityData;
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=3d51ff48e9ee8482ed210ebd0d533bdc';
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data)
+        cityData = data;
+        for (var i = 0; i < data.length; i++) {
+            //Create a list element
+            var listItem = document.createElement('li');
+            listItem.classList.add("list-group-item");
+            //Set the text of the list element to the JSON response's .html_url property
+            var content = (data[i].name + ', ' + data[i].state);
+            listItem.textContent = content;
+            listItem.setAttribute("lon", data[i].lon);
+            listItem.setAttribute("lat", data[i].lat);
+            listItem.setAttribute("name", data[i].name);
+            listItem.setAttribute("state", data[i].state);
+            //Append the li element to the id associated with the ul element.
+            cityList.appendChild(listItem);
+          }
+          fillForecast(data[0].lat, data[0].lon);
     });
+    // populate the weather section with the data.
+
     event.preventDefault();
 })
+
+// when a user clicks on one of the saved cities, it populates the weather items.
